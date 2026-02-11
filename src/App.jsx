158b4350971e226cbdcda53af4791f72957a5b21
roshitha <roshitha427@gmail.com>
2026-02-11 12:44:57 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
-import Home from "./Components/Home";
-import Login from "./Components/Login";
-import Dashboard from "./Components/Dashboard";
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <div>
+      <h1>welcome guys</h1>
+    </div>
+  );
 }
 
 export default App;
